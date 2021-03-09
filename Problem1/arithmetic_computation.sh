dab72eb2d03bb1b8adb3156fca7ba831f30b1b5f
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-09 15:01:58 +0530

    [SAI] added the second computation lines to the file

--- Problem1/arithmetic_computation.sh
+++ Problem1/arithmetic_computation.sh
@@ -4,3 +4,5 @@ read -p "What is the second number you want to give" second_number_b
 read -p "Wha ti s the third number you want to to give" third_number_c
 first_computation=$(( first_number_a + second_number_b * third_number_c ))
 echo $first_computation
+second_computation=$(( first_number_a * second_number_b + third_number_c ))
+echo $second_computation
