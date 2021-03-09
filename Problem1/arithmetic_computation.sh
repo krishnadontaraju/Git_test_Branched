dbb4877405827ea630c63843df2bafa4b7104037
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-09 15:31:40 +0530

    [SAI] added the computation lines to the array and edited file

--- Problem1/arithmetic_computation.sh
+++ Problem1/arithmetic_computation.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
+
 declare -A storage_dict
+declare -a storage_array
 read -p "What is the first number you want to give" first_number_a
 read -p "What is the second number you want to give" second_number_b
 read -p "What is the third number you want to to give" third_number_c
@@ -9,4 +11,8 @@ storage_dict[1]=$(( first_number_a * second_number_b + third_number_c ))
 storage_dict[2]=$(( third_number_c + first_number_a / second_number_b ))
 storage_dict[3]=$(( first_number_a % second_number_b + third_number_c ))
 
-echo ${storage_dict[@]}
+for i in ${!storage_dict[@]}
+do
+	storage_array[$i]=${storage_dict[$i]}
+done
+echo ${storage_array[@]}
