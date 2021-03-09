0d9df0f11afdaa1b7571b361fc8f4bb0f027121d
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-09 15:18:43 +0530

    [SAI] added the computation lines to the dictionary and edited the file

--- Problem1/arithmetic_computation.sh
+++ Problem1/arithmetic_computation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+declare -A storage_dict
 read -p "What is the first number you want to give" first_number_a
 read -p "What is the second number you want to give" second_number_b
-read -p "Wha ti s the third number you want to to give" third_number_c
-first_computation=$(( first_number_a + second_number_b * third_number_c ))
-echo $first_computation
-second_computation=$(( first_number_a * second_number_b + third_number_c ))
-echo $second_computation
-third_computaution=$(( third_number_c + first_number_a / second_number_b ))
-echo $third_computation
-fourth_computation=$(( first_number_a % second_number-b + third_number_c ))
-echo $fourth_computation
+read -p "What is the third number you want to to give" third_number_c
+
+storage_dict[0]=$(( first_number_a + second_number_b * third_number_c ))
+storage_dict[1]=$(( first_number_a * second_number_b + third_number_c ))
+storage_dict[2]=$(( third_number_c + first_number_a / second_number_b ))
+storage_dict[3]=$(( first_number_a % second_number_b + third_number_c ))
+
+echo ${storage_dict[@]}
