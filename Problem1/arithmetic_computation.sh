007a799381c0e3c17d9a50ccd8f2d67bfe1c1e76
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-09 14:55:41 +0530

    [SAI] added the read lines to the file

--- Problem1/arithmetic_computation.sh
+++ Problem1/arithmetic_computation.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-
+read -p "What is the first number you want to give" first_number_a
+read -p "What is the second number you want to give" second_number_b
+read -p "Wha ti s the third number you want to to give" third_number_c
